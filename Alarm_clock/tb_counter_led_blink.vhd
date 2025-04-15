428e05aa351f1b88c031bee2b39a4d4f09da541e
Muhammad Waqas Umair <waqasumair111@gmail.com>
2025-04-16 01:42:18 +0200

    reorganize

--- Alarm_clock/tb_counter_led_blink.vhd
+++ Alarm_clock/tb_counter_led_blink.vhd
@@ -3,98 +3,86 @@
 -- Engineer:
 --
 -- Create Date:   18:50:27 07/28/2023
--- Design Name:   
--- Module Name:   C:/Users/umair khan/Videos/Desktop/4th semestter+cmp/vhdl/Alarm_clock1/tb_counter_led_blink.vhd
+-- Design Name:   LED Blink Counter Testbench
+-- Module Name:   tb_counter_led_blink
 -- Project Name:  Alarm_clock1
--- Target Device:  
--- Tool versions:  
--- Description:   
--- 
--- VHDL Test Bench Created by ISE for module: counter_led_blink
--- 
--- Dependencies:
--- 
+-- Description:   Testbench for 2Hz LED blinker module
+--
+-- Dependencies:  counter_led_blink.vhd
+--
 -- Revision:
 -- Revision 0.01 - File Created
 -- Additional Comments:
---
--- Notes: 
--- This testbench has been automatically generated using types std_logic and
--- std_logic_vector for the ports of the unit under test.  Xilinx recommends
--- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the testbench will bind correctly to the post-implementation 
--- simulation model.
+--   Tests 2Hz blinking functionality during snooze mode
 --------------------------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
- 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---USE ieee.numeric_std.ALL;
- 
+
 ENTITY tb_counter_led_blink IS
 END tb_counter_led_blink;
- 
+
 ARCHITECTURE behavior OF tb_counter_led_blink IS 
- 
-    -- Component Declaration for the Unit Under Test (UUT)
- 
+
+    -- Component Declaration for Unit Under Test (UUT)
     COMPONENT counter_led_blink
     PORT(
-         clk : IN  std_logic;
-         snooze_act : IN  std_logic;
-         reset : IN  std_logic;
-         led_blink_2Hz : OUT  std_logic
-        );
+        clk           : IN  std_logic;
+        snooze_act    : IN  std_logic;
+        reset         : IN  std_logic;
+        led_blink_2Hz : OUT std_logic
+    );
     END COMPONENT;
-    
 
-   --Inputs
-   signal clk : std_logic := '0';
-   signal snooze_act : std_logic := '0';
-   signal reset : std_logic := '0';
+    -- Input signals
+    signal clk        : std_logic := '0';
+    signal snooze_act : std_logic := '0';
+    signal reset      : std_logic := '0';
 
- 	--Outputs
-   signal led_blink_2Hz : std_logic;
+    -- Output signal
+    signal led_blink_2Hz : std_logic;
 
-   -- Clock period definitions
-   constant clk_period : time := 0.1 ms;
+    -- Clock period definitions (0.1ms = 10kHz clock)
+    constant clk_period : time := 0.1 ms;
  
 BEGIN
- 
-	-- Instantiate the Unit Under Test (UUT)
-   uut: counter_led_blink PORT MAP (
-          clk => clk,
-          snooze_act => snooze_act,
-          reset => reset,
-          led_blink_2Hz => led_blink_2Hz
-        );
+    -- Instantiate the Unit Under Test (UUT)
+    uut: counter_led_blink PORT MAP(
+        clk           => clk,
+        snooze_act    => snooze_act,
+        reset         => reset,
+        led_blink_2Hz => led_blink_2Hz
+    );
 
-   -- Clock process definitions
-   clk_process :process
-   begin
-		clk <= '0';
-		wait for clk_period/2;
-		clk <= '1';
-		wait for clk_period/2;
-   end process;
- 
+    -- Clock process (10kHz clock)
+    clk_process: process
+    begin
+        clk <= '0';
+        wait for clk_period/2;
+        clk <= '1';
+        wait for clk_period/2;
+    end process;
 
-   -- Stimulus process
-   stim_proc: process
-   begin		
-      -- hold reset state for 100 ns.
-   
-
-      wait for clk_period;
-		reset<= '0';
-		snooze_act<= '1';
-		wait for 1000 ms;
-
-      -- insert stimulus here 
-
-      wait;
-   end process;
+    -- Stimulus process
+    stim_proc: process
+    begin
+        -- Initial reset
+        reset <= '1';
+        wait for clk_period;
+        reset <= '0';
+        
+        -- Test 1
+        snooze_act <= '1';
+        wait for 1000 ms;  
+        
+        -- Test 2
+        snooze_act <= '0';
+        wait for 200 ms;
+        
+        -- Test 3
+        snooze_act <= '1';
+        wait for 500 ms;
+        
+        wait;
+    end process;
 
 END;
-
--- Alarm_clock/tb_set_time_function.vhd
+++ Alarm_clock/tb_set_time_function.vhd
@@ -3,107 +3,100 @@
 -- Engineer:
 --
 -- Create Date:   17:44:06 07/12/2023
--- Design Name:   
--- Module Name:   C:/Users/umair khan/Videos/Desktop/4th semestter/vhdl/Alarm_clock1/tb_set_time_function.vhd
+-- Design Name:   Time Setting Function Testbench
+-- Module Name:   tb_set_time_function
 -- Project Name:  Alarm_clock1
--- Target Device:  
--- Tool versions:  
--- Description:   
--- 
--- VHDL Test Bench Created by ISE for module: Set_time_function
--- 
--- Dependencies:
--- 
+-- Description:   Testbench for time setting functionality
+--
+-- Dependencies:  Set_time_function.vhd
+--
 -- Revision:
 -- Revision 0.01 - File Created
 -- Additional Comments:
---
--- Notes: 
--- This testbench has been automatically generated using types std_logic and
--- std_logic_vector for the ports of the unit under test.  Xilinx recommends
--- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the testbench will bind correctly to the post-implementation 
--- simulation model.
+--   Tests time increment/decrement functionality in setting mode
 --------------------------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
- 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---USE ieee.numeric_std.ALL;
- 
+
 ENTITY tb_set_time_function IS
 END tb_set_time_function;
- 
+
 ARCHITECTURE behavior OF tb_set_time_function IS 
- 
-    -- Component Declaration for the Unit Under Test (UUT)
- 
+
+    -- Component Declaration for Unit Under Test (UUT)
     COMPONENT Set_time_function
     PORT(
-         clock : IN  std_logic;
-         reset : IN  std_logic;
-         alarm_mode : IN  std_logic_vector(2 downto 0);
-         key_enable : IN  std_logic;
-         key_plus_minus : IN  std_logic;
-         set_hour : OUT  std_logic_vector(4 downto 0);
-         set_minute : OUT  std_logic_vector(5 downto 0);
-         set_second : OUT  std_logic_vector(5 downto 0)
-        );
+        clock          : IN  std_logic;
+        reset          : IN  std_logic;
+        alarm_mode     : IN  std_logic_vector(2 downto 0);
+        key_enable     : IN  std_logic;
+        key_plus_minus : IN  std_logic;
+        set_hour       : OUT std_logic_vector(4 downto 0);
+        set_minute     : OUT std_logic_vector(5 downto 0);
+        set_second     : OUT std_logic_vector(5 downto 0)
+    );
     END COMPONENT;
-    
 
-   --Inputs
-   signal clock : std_logic := '0';
-   signal reset : std_logic := '0';
-   signal alarm_mode : std_logic_vector(2 downto 0) := "000";
-   signal key_enable : std_logic := '0';
-   signal key_plus_minus : std_logic := '0';
+    -- Input signals
+    signal clock          : std_logic := '0';
+    signal reset          : std_logic := '0';
+    signal alarm_mode     : std_logic_vector(2 downto 0) := "000";
+    signal key_enable     : std_logic := '0';
+    signal key_plus_minus : std_logic := '0';
 
- 	--Outputs
-   signal set_hour : std_logic_vector(4 downto 0);
-   signal set_minute : std_logic_vector(5 downto 0);
-   signal set_second : std_logic_vector(5 downto 0);
+    -- Output signals
+    signal set_hour       : std_logic_vector(4 downto 0);
+    signal set_minute     : std_logic_vector(5 downto 0);
+    signal set_second     : std_logic_vector(5 downto 0);
 
-   -- Clock period definitions
-   constant clock_period : time := 10 ns;
+    -- Clock period definitions
+    constant clock_period : time := 10 ns;
  
 BEGIN
- 
-	-- Instantiate the Unit Under Test (UUT)
-   uut: Set_time_function PORT MAP (
-          clock => clock,
-          reset => reset,
-          alarm_mode => alarm_mode,
-          key_enable => key_enable,
-          key_plus_minus => key_plus_minus,
-          set_hour => set_hour,
-          set_minute => set_minute,
-          set_second => set_second
-        );
+    -- Instantiate the Unit Under Test (UUT)
+    uut: Set_time_function PORT MAP(
+        clock          => clock,
+        reset          => reset,
+        alarm_mode     => alarm_mode,
+        key_enable     => key_enable,
+        key_plus_minus => key_plus_minus,
+        set_hour       => set_hour,
+        set_minute     => set_minute,
+        set_second     => set_second
+    );
 
-   -- Clock process definitions
-   clock_process :process
-   begin
-		clock <= '0';
-		wait for clock_period/2;
-		clock <= '1';
-		wait for clock_period/2;
-   end process;
- 
-
-   -- Stimulus process
-  process  
-  begin
-     
-    reset <= '0';
-    key_enable <= '1';
-    key_plus_minus <= '0';
-	 alarm_mode <= "010";
-    
-    
-  
-    wait;
+    -- Clock process
+    clock_process: process
+    begin
+        clock <= '0';
+        wait for clock_period/2;
+        clock <= '1';
+        wait for clock_period/2;
     end process;
 
-end Behavior;
+    -- Stimulus process
+    stim_proc: process
+    begin
+        -- Initialize with reset
+        reset <= '1';
+        wait for clock_period;
+        reset <= '0';
+        
+        -- Test case 1
+        alarm_mode <= "010";  
+        key_enable <= '1';    
+        key_plus_minus <= '0'; 
+        wait for 10*clock_period;
+        
+        -- Test case 2
+        key_plus_minus <= '1'; 
+        wait for 10*clock_period;
+        
+        -- Test case 3
+        alarm_mode <= "000";  
+        wait for 5*clock_period;
+        
+        wait;
+    end process;
+
+END behavior;
