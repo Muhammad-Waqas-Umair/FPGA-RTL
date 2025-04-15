df75ab8e6f9d2daa9c57d35675d12d2582daf3dc
Muhammad Waqas Umair <waqasumair111@gmail.com>
2025-04-16 01:13:39 +0200

    Update tb_addop.vhd

--- IDEA_Direct/tb_addop.vhd
+++ IDEA_Direct/tb_addop.vhd
@@ -3,75 +3,65 @@
 -- Engineer:
 --
 -- Create Date:   01:35:14 05/26/2023
--- Design Name:   
--- Module Name:   C:/Users/umair khan/Videos/Desktop/direct/tb_addop.vhd
+-- Design Name:   Testbench for 16-bit Adder
+-- Module Name:   tb_addop
 -- Project Name:  idea
--- Target Device:  
--- Tool versions:  
--- Description:   
+-- Description:   Testbench for verifying the addop module functionality
 -- 
--- VHDL Test Bench Created by ISE for module: addop
--- 
--- Dependencies:
--- 
+-- Dependencies:  addop.vhd
+--
 -- Revision:
 -- Revision 0.01 - File Created
 -- Additional Comments:
---
--- Notes: 
--- This testbench has been automatically generated using types std_logic and
--- std_logic_vector for the ports of the unit under test.  Xilinx recommends
--- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the testbench will bind correctly to the post-implementation 
--- simulation model.
+--   Tests both basic and overflow cases for 16-bit addition
 --------------------------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
-USE IEEE.std_logic_unsigned.all; 
- 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---USE ieee.numeric_std.ALL;
- 
+USE IEEE.std_logic_unsigned.ALL; 
+
 ENTITY tb_addop IS
 END tb_addop;
- 
+
 ARCHITECTURE behavior OF tb_addop IS 
- 
     -- Component Declaration for the Unit Under Test (UUT)
- 
     COMPONENT addop
     PORT(
-         A : IN  std_logic_vector(15 downto 0);
-         B : IN  std_logic_vector(15 downto 0);
-         C : OUT  std_logic_vector(15 downto 0)
-        );
+        A : IN  std_logic_vector(15 downto 0);
+        B : IN  std_logic_vector(15 downto 0);
+        C : OUT std_logic_vector(15 downto 0)
+    );
     END COMPONENT;
-    
 
-   --Inputs
-   signal A : std_logic_vector(15 downto 0) := (others => '0');
-   signal B : std_logic_vector(15 downto 0) := (others => '0');
+    -- Input signals
+    signal A : std_logic_vector(15 downto 0) := (others => '0');
+    signal B : std_logic_vector(15 downto 0) := (others => '0');
 
- 	--Outputs
-   signal C : std_logic_vector(15 downto 0);
-   
- 
+    -- Output signal
+    signal C : std_logic_vector(15 downto 0);
+
 BEGIN
- 
-	-- Instantiate the Unit Under Test (UUT)
-   uut: addop PORT MAP (
-          A => A,
-          B => B,
-          C => C
-        );
+    -- Instantiate the Unit Under Test (UUT)
+    uut: addop PORT MAP (
+        A => A,
+        B => B,
+        C => C
+    );
 
- 
+    -- Stimulus process
+    stimulus: process
+    begin
+        -- Test case 1: Zero addition
+        A <= "0000000000000000";
+        B <= "1111111111111111";
+        wait for 200 ns;
+        
+        -- Test case 2: Non-zero addition with potential overflow
+        A <= "1000111100000000";  -- 0x8F00
+        B <= "0100110011100011";  -- 0x4CE3
+        wait for 200 ns;
+        
+        -- Add more test cases here if needed
+        wait;
+    end process stimulus;
 
-   -- Stimulus process
-    A<="0000000000000000", "1000111100000000" after 200ns ;
-    B<="1111111111111111", "0100110011100011" after 200ns;
-			  
-  
-
-END;
+END behavior;
